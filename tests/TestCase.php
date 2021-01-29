226d020d92541b4ced09fd9a0b400f252081fbbe
Andrey Helldar <andrey-helldar@users.noreply.github.com>
2021-01-29 23:46:34 +0000

    Apply fixes from StyleCI

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -25,14 +25,14 @@ abstract class TestCase extends BaseTestCase
     /** @var \Symfony\Bundle\FrameworkBundle\Console\Application */
     protected $application;
 
-    abstract protected function getSyncConfig(): ?array;
-
     protected function setUp(): void
     {
         $this->mockContainer();
         $this->mockApplication();
         $this->mockCommand();
     }
+
+    abstract protected function getSyncConfig(): ?array;
 
     protected function mockContainer(): void
     {
